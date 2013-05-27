64bcbac04d8f136c87ccd2f74a8be076c4fc48a0
ckretaznman <ckretaznman378@gmail.com>
2013-05-27 13:42:19 -0400

    New project
    Adding rails

---
+++ ping_on/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PingOn::Application.config.secret_token = '0e3fe0736bcce3980cc8faf420d3f5cd75528f705c4981b211e4e37fab8543825cf6086ed18cfe138bee0dd29cf8a25eebd88d6b573c8531f6e5df65d116ffb3'
