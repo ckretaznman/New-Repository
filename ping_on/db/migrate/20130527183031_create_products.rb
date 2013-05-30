576ba76f384d7191f8516db2eafaa07e52d353bc
ckretaznman <ckretaznman378@gmail.com>
2013-05-30 11:35:40 -0400

    a

--- ping_on/db/migrate/20130527183031_create_products.rb
+++ ping_on/db/migrate/20130527183031_create_products.rb
@@ -3,5 +3,12 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :name, :null => false
       t.text :description
+      t.column(:name, :string)
   end
+  end
+
+  def change
+  	    add_column :products, :part_number, :string
+  	    add_column :products, :price, :decimal
+  	end
 end
